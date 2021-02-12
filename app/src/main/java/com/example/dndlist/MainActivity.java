bba7df1eb90246daa72fbb680ea93be26ded9b73
Ishutin Ilya Dmitrievich <rollstain@ya.ru>
2021-02-12 20:48:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dndlist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dndlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
